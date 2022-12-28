ded467d7d941666dca18fb47b34e58e006ada514
Fabio Dias <fabiodias11@usp.br>
2022-12-28 02:17:54 -0300

    Eventos e lógicas

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
+
+    if(elemento && elemento.localName === 'audio') {
+            elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado ou seletor inválido');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
 //para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;//template string
@@ -17,4 +22,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     };
 
+    tecla.onkeydown = function(evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
 }
